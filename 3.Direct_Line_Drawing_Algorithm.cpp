8a27df2d5ff5eecfadefdad948c1451cbaa89142
Karimhossion <karimhossion2012@gmail.com>
2025-04-14 22:52:37 +0600

    karim

--- 3.Direct_Line_Drawing_Algorithm.cpp
+++ 3.Direct_Line_Drawing_Algorithm.cpp
@@ -1,60 +1,45 @@
-#include<windows.h>
+#include <windows.h>
 #include <GL/glut.h>
-#include<iostream>
+#include <cmath>
+#include <iostream>
 using namespace std;
-float x1, x2, yStart, yEnd;
-void display(void)
+int x_start,y_start,x_end,y_end;
+void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
     glBegin(GL_LINES);
-    glVertex2f(0.0, 1.0);
-    glVertex2f(0.0, -1.0);
-    glVertex2f(1.0, 0.0);
-    glVertex2f(-1.0, 0.0);
+    glVertex2i(0,100);
+    glVertex2i(0,-100);
+    glVertex2i(100,0);
+    glVertex2i(-100,0);
     glEnd();
-    glColor3f(1.0,0.0,0.0);
     glBegin(GL_POINTS);
-    float xstart = 0.1, ystart = 0.1;
-    float xend = 0.5, yend = 0.8;
-    float x = xstart, y = ystart;
-    float m = (yend - ystart)/(xend - xstart);
-    float b = ystart - (m * xstart);
-    while (x <= xend)
+    float x=x_start;
+    float y=y_start;
+    float m = (y_end - y_start)/(x_end - x_start);
+    float b = (y_start - (m*x_start));
+
+    while(x<=x_end)
     {
-        glVertex2f(x, y);
-        x += 0.01;
-        y = (m * x) + b;
-    }
-    glColor3f(0.0,0.0,1.0);
-    xstart = (x1 < x2) ? x1 : x2;
-    ystart = (yStart < yEnd) ? yStart : yEnd;
-    xend = (x1 > x2)?x1:x2;
-    yend = (yStart>yEnd)?yStart:yEnd;
-    x = xstart;
-    y = ystart;
-    m = (yend - ystart) / (xend - xstart);
-    b = ystart - (m * xstart);
-    while (x <= xend)
-    {
-        glVertex2f(x, y);
-        x += 0.01;
-        y = (m * x) + b;
+        glVertex2i(x,floor(y+0.5));
+        x=x+1;
+        y=((m*x) + b);
     }
     glEnd();
     glFlush();
 }
 int main(int argc, char *argv[])
 {
-    cout << "Enter (x1, y1): ";
-    cin >> x1 >> yStart;
-    cout << "Enter (x2, y2): ";
-    cin >> x2 >> yEnd;
+    cout<<"Enter (x_start, y_start) = ";
+    cin>>x_start>>y_start;
+    cout<<"Enter (x_end, y_end) = ";
+    cin>>x_end>>y_end;
     glutInit(&argc, argv);
-    glutInitWindowSize(640, 480);
-    glutInitWindowPosition(10, 10);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
     glutCreateWindow("GLUT Shapes");
+    gluOrtho2D(-10,10,-10,10);
     glutDisplayFunc(display);
     glutMainLoop();
     return EXIT_SUCCESS;
