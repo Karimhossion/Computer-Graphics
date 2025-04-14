64cafcddb1ba645c35c4de102ea8fbf04e0cac9d
Karimhossion <karimhossion2012@gmail.com>
2025-04-14 23:53:44 +0600

    karim

--- 2.1.vertical_line.cpp
+++ 2.1.vertical_line.cpp
@@ -10,25 +10,24 @@ void display()
     glVertex2f(-1.,0.0);
     glEnd();
     glBegin(GL_POINTS);
-    float x_start=-0.5, x_end=-0.1,y=0.3;
-    float x=x_start;
-    while(x<=x_end)
+    float x=0.3,y_start=-0.5,y_end=0.5;
+    float y=y_start;
+    while(y<=y_end)
     {
         glVertex2f(x,y);
-        x=x+0.01;
+        y=y+0.01;
     }
     glEnd();
     glFlush();
-
-
 }
+
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
     glutInitWindowSize(640,480);
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
-    glutCreateWindow("v line algorithm");
+    glutCreateWindow("GLUT Shapes");
     glutDisplayFunc(display);
     glutMainLoop();
 
